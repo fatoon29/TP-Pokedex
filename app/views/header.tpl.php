97927fa3ec24b108845d68a9cd54a823099ce6d2
fatoon29 <fab.perrichon@gmail.com>
2020-01-18 00:32:17 +0100

    style+

--- app/views/header.tpl.php
+++ app/views/header.tpl.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello</title>
+    <title>O'Pokedex</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Bree+Serif&display=swap" rel="stylesheet">
@@ -14,10 +14,11 @@
 </head>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand" href="<?= $router->generate("home") ?>">
-        <img src="<?= $_SERVER["BASE_URI"] ?>/img/pokedex.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        O'Pokedex
+<a class="navbar-brand" href="<?= $router->generate("home") ?>" >
+        <img headerImg src="<?= $_SERVER["BASE_URI"] ?>/img/pokedex.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        
     </a>
+    <h1>O'Pokedex</h1>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -1,22 +1,15 @@
 <div class="card-deck">
-
-
-
     <?php foreach ($viewParams['list'] as $pokemon) : ?>
         <div class="col mb-6">
             <div class="card">
-                <img src="<?= $_SERVER["BASE_URI"] ?>/img/<?= $pokemon->getNumero() ?>.png" alt= <?= $pokemon->getNom() ?>>
+                <img src="<?= $_SERVER["BASE_URI"] ?>/img/<?= $pokemon->getNumero() ?>.png" alt=<?= $pokemon->getNom() ?>>
                 <div class="card-body">
-                    <h5 class="card-title"><?= $pokemon->getNumero() ?></h5>
-                    <h5 class="card-title"><?= $pokemon->getNom() ?></h5>
-                   
-                    <a href="<?= $router->generate("details", ["id" => $pokemon->getId()]) ?>"
-                    class="btn btn-primary">Détails du pokemon</a>
+                    <p class="card-text">#<?= $pokemon->getNumero() ?> <?= $pokemon->getNom() ?></p>
+                </div>
+                <div class="card-footer">
+                    <a href="<?= $router->generate("details", ["id" => $pokemon->getId()]) ?>" class="btn btn-primary">Détails du pokemon</a>
                 </div>
             </div>
         </div>
-
     <?php endforeach ?>
-
-
 </div>
